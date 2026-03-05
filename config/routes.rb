588c7fa1570da8c42855ccc37fa128edc5936361
Angel <akohli2@rrc.ca>
2026-03-05 11:51:18 -0600

    Add customers controller with index, alphabetized and missing_email views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root 'customers#index'
+  get '/customers/missing_email', to: 'customers#missing_email'
+  get '/customers/alphabetized', to: 'customers#alphabetized'
 end
